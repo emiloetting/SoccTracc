b2c09de8f808c2deb0203e802f92e63f28e6012d
Emil Ötting <emil.oetting@gmx.de>
2025-03-26 13:20:10 +0100

    Add file for testing doc-creation

---
+++ pipeline/doc_tester.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def tester(arg_1: int=5, arg_2: str='Hello') -> np.ndarray:
+    """Tester function for documentation purposes.
+    "
+    " Parameters
+    " ----------"
+    " arg_1 : int, optional"
+    "     An integer argument. Default is 5.""
+    " arg_2 : str, optional""
+    "     A string argument. Default is 'Hello'.""
+    ""
+    " Returns"
+    " -------""
+    " np.ndarray"
+    """
+    return np.array([arg_1, arg_2])
+    
