4e3250a88f864b5152aae82b6c8577e1e225b0ba
Emil Ötting <emil.oetting@gmx.de>
2025-03-26 13:50:08 +0100

    change docstring

--- pipeline/doc_tester.py
+++ pipeline/doc_tester.py
@@ -2,17 +2,13 @@ import numpy as np
 
 def tester(arg_1: int=5, arg_2: str='Hello') -> np.ndarray:
     """Tester function for documentation purposes.
-    "
-    " Parameters
-    " ----------"
-    " arg_1 : int, optional"
-    "     An integer argument. Default is 5.""
-    " arg_2 : str, optional""
-    "     A string argument. Default is 'Hello'.""
-    ""
-    " Returns"
-    " -------""
-    " np.ndarray"
+
+    Args:
+        arg_1 (int, optional): An integer. Defaults to 5.
+        arg_2 (str, optional): A string. Defaults to 'Hello'.
+    
+    Returns:
+        np.ndarray: A numpy array containing arg_1 and arg_2.
     """
     return np.array([arg_1, arg_2])
     
