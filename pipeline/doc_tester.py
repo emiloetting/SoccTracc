c5be2d2c74bb227984e82aac4298324241055428
Emil Ötting <emil.oetting@gmx.de>
2025-07-10 19:37:34 +0200

    Linted with black

--- pipeline/doc_tester.py
+++ pipeline/doc_tester.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-def tester(arg_1: int=5, arg_2: str='Hello') -> np.ndarray:
+
+def tester(arg_1: int = 5, arg_2: str = "Hello") -> np.ndarray:
     """Tester function for documentation purposes.
 
     Args:
         arg_1 (int, optional): An integer. Defaults to 5.
         arg_2 (str, optional): A string. Defaults to 'Hello'.
-    
+
     Returns:
         np.ndarray: A numpy array containing arg_1 and arg_2.
     """
     return np.array([arg_1, arg_2])
 
-def calculate_discounted_price ( price: float, discount_rate: float ) -> float:
+
+def calculate_discounted_price(price: float, discount_rate: float) -> float:
     """
     TO CHECK LINTER: Calculate the discounted price given the original price and discount rate.
 
@@ -26,8 +28,7 @@ def calculate_discounted_price ( price: float, discount_rate: float ) -> float:
     Raises:
       ValueError: If the price is negative.
     """
-    if( price < 0 ):
-         raise ValueError("Price cannot be negative")
-    discounted_price: float = price - ( price * discount_rate )
+    if price < 0:
+        raise ValueError("Price cannot be negative")
+    discounted_price: float = price - (price * discount_rate)
     return discounted_price
-    
